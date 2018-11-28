a7dbff8dd31323b7fe6bd590bd4b9dee5cb752b9
Choi Dong Min <35302641+ChoiDM@users.noreply.github.com>
2018-11-28 13:36:45 +0900

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,6 +1,7 @@
 from .segnet import SegNet
 from .unet import Unet
 from .ternausnet import TernausNet
+from .deeplab_v3 import 
 
 
 def get_network(name, num_class):
@@ -11,5 +12,7 @@ def get_network(name, num_class):
         return Unet(num_class)
     elif name == 'ternausnet':
         return TernausNet(num_class)
+    elif name == 'deeplabv3+':
+        return Deeplab_v3_plus(input_channels = 3, n_classes = num_class)
     raise ValueError
 
